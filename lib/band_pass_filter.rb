f3934c8a3941faaf80bd1e8ad93797a59921302d
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-10-01 14:23:35 +0100

    passing all simple tests

--- lib/band_pass_filter.rb
+++ lib/band_pass_filter.rb
@@ -1,3 +1,9 @@
 def frequency_filter(track, lower_limit, upper_limit)
+  if track[0] < lower_limit
+    track[0] = lower_limit
+  elsif track [0] > upper_limit
+    track[0] = upper_limit
+  end
 
+  track
 end
--- spec/band_pass_filter_spec.rb
+++ spec/band_pass_filter_spec.rb
@@ -4,4 +4,12 @@ describe '#frequency_filter' do
   it 'returns array unchanged when within limits' do
     expect(frequency_filter([200], 100, 300)).to eq [200]
   end
+
+  it 'returns changed array when below lower limit' do
+    expect(frequency_filter([50], 100, 300)).to eq [100]
+  end
+
+  it 'returns changed array when above upper limit' do
+    expect(frequency_filter([350], 100, 300)).to eq [300]
+  end
 end

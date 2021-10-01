f8864fdae63125024d6b30002804e22ff8ae4a78
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-10-01 14:30:33 +0100

    passing all tests bar edge cases

--- lib/band_pass_filter.rb
+++ lib/band_pass_filter.rb
@@ -1,9 +1,11 @@
 def frequency_filter(track, lower_limit, upper_limit)
-  if track[0] < lower_limit
-    track[0] = lower_limit
-  elsif track [0] > upper_limit
-    track[0] = upper_limit
+  modified_track = []
+
+  track.each do |frequency|
+    frequency = lower_limit if frequency < lower_limit
+    frequency = upper_limit if frequency > upper_limit
+    modified_track << frequency
   end
 
-  track
+  modified_track
 end
--- spec/band_pass_filter_spec.rb
+++ spec/band_pass_filter_spec.rb
@@ -12,4 +12,10 @@ describe '#frequency_filter' do
   it 'returns changed array when above upper limit' do
     expect(frequency_filter([350], 100, 300)).to eq [300]
   end
+
+  # [450, 600, 120, 250, 300], 200, 500 => [450, 500, 200, 250, 300]
+
+  it 'returns changed array with values below lower limit and above upper limit' do
+    expect(frequency_filter([450, 600, 120, 250, 300], 200, 500)).to eq [450, 500, 200, 250, 300]
+  end
 end
